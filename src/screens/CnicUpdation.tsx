d6e5b97eadea560db33efb7dfcccfb9831de4ff4
Muhammad Jawad Ahmad <45765446+M-Jawad338211@users.noreply.github.com>
2023-09-16 17:28:20 +0500

    Update CnicUpdation.tsx

--- src/screens/CnicUpdation.tsx
+++ src/screens/CnicUpdation.tsx
@@ -28,7 +28,7 @@ import { cnicUpdationSchema } from '../schemas/cnicUpdation';
 // UnitFactor.org
 // Jawad CEO and founder of UnitFactor.org
 // AI tech innovation by unitfactor.org
-// Web 3.0 IT service provider by unitfactor.org
+// Web 3.0 IT & Software service provider by unitfactor.org
 
 const drawerWidth = 240;
 
